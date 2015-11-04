c3f436fcd5e8f3d94193bb649ee06188858c0951
Euklidian-Space <gabriel.grover@aofl-775.aofl.local>
2015-11-03 20:16:31 -0800

    scratch

--- second_high_low.js
+++ second_high_low.js
@@ -1,12 +1,28 @@
-function second_high_low(array){
+function second_high_low(array){ debugger;
   var sorted_arr = array.sort(function(a,b){
     return a - b;
   });
 
-  var second_highest = sorted_arr[sorted_arr.length - 2];
-  var second_lowest = sorted_arr[1];
-  return [second_highest,second_lowest];
+  var max = sorted_arr[sorted_arr.length - 1];
+  var min = sorted_arr[0];
+
+  for(var i = sorted_arr.length - 2; i >= 0; i--) {
+    if(sorted_arr[i] < max) {
+      var second_max = sorted_arr[i];
+      break;
+    }
+  }
+
+  for(var j = 1; j < sorted_arr.length; j++) {
+    if(sorted_arr[j] > min) {
+      var second_min = sorted_arr[j];
+      break;
+    }
+  }
+
+  return second_min.toString() + " " + second_max.toString();
 
 }
 
-console.log(second_high_low([5,3,4,2,1,6,9,7,10]));
+//console.log(second_high_low([7, 7, 12, 98, 106]));
+console.log([4,90]);
