583edf2147f825b10cafaf18f26371cf25115308
Euklidian-Space <gabriel.grover@aofl-775.aofl.local>
2015-11-06 09:30:41 -0800

    fixed div string…sort of

--- Division_string.js
+++ Division_string.js
@@ -1,21 +1,21 @@
 function DivisionStringified(num1,num2) { //debugger;
 
   // code goes here
-  var quotient = Math.floor(num1/num2);
+  var quotient = Math.round(num1/num2);
   var q_str = quotient.toString();
   var q_str2 = q_str.split("");
   if(num1 <= num2) {
-    return quotient.toString();
+    return Math.floor(num1/num2).toString();
   }
 
   if(q_str.length > 3) {
-  	for(var i = q_str.length - 3; i >= 0; i -= 4){
+  	for(var i = q_str.length - 3; i >= 0; i -= 3){
    	  q_str2.splice(i,0,",");
     }
 
     return q_str2.join("");
   } else {
-  	return Math.ceil(num1/num2);
+  	return Math.ceil(num1/num2).toString();
   }
 
 //  return q_str2;
@@ -30,6 +30,6 @@ function DivisionStringified(num1,num2) { //debugger;
 
 
 //console.log(DivisionStringified(1,10));
-//console.log(DivisionStringified(503394930, 43));
+console.log(DivisionStringified(503394930, 43));
 //console.log(DivisionStringified(5,54));
-console.log(DivisionStringified(63,14));
+//console.log(DivisionStringified(63,14));
